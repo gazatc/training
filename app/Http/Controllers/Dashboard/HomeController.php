3d3f6d40077a89a85178b949af3836d2eac3d308
Abdullah Shublaq <abd.shublaq@gmail.com>
2021-04-16 15:33:51 +0300

    statistics

--- app/Http/Controllers/Dashboard/HomeController.php
+++ app/Http/Controllers/Dashboard/HomeController.php
@@ -4,12 +4,23 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Actor;
 use App\Admin;
+use App\Application;
 use App\Category;
+use App\Employer;
+use App\EmployerVerify;
 use App\Film;
 use App\Http\Controllers\Controller;
+use App\Inquire;
+use App\Job;
+use App\JobSeeker;
+use App\JobSeekerVerify;
 use App\Message;
 use App\Rating;
+use App\Region;
 use App\Review;
+use App\Role;
+use App\Team;
+use App\Training;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -18,7 +29,19 @@ class HomeController extends Controller
     //
     public function index(){
         $admins = Admin::count();
+        $roles = Role::count();
+        $categories = Category::count();
+        $regions = Region::count();
+        $employers = Employer::count();
+        $jobSeekers = JobSeeker::count();
+        $jobs = Job::count();
+        $trainings = Training::count();
+        $teams = Team::count();
+        $applications = Application::count();
+        $inquires = Inquire::count();
+        $messages = Message::count();
 
-        return view('dashboard.home', compact('admins'));
+        return view('dashboard.home',
+            compact('admins', 'roles', 'categories', 'regions', 'employers', 'jobSeekers', 'jobs', 'trainings', 'teams', 'applications', 'inquires', 'messages'));
     }
 }
--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -19,7 +19,7 @@
         </div>
         <div class="container-fluid">
             <div class="row clearfix">
-                <div class="col-lg-4 col-md-6">
+                <div class="col-lg-3 col-md-6">
                     <div class="card top_counter">
                         <div class="body">
                             <div class="icon xl-slategray"><i class="zmdi zmdi-account"></i> </div>
@@ -30,48 +30,127 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6">
+                <div class="col-lg-3 col-md-6">
                     <div class="card top_counter">
                         <div class="body">
-                            <div class="icon xl-slategray"><i class="zmdi zmdi-account-circle"></i> </div>
+                            <div class="icon xl-slategray"><i class="zmdi zmdi-key"></i> </div>
                             <div class="content">
-                                <div class="text font-20">الأشخاص</div>
-                                <h5 class="number">{{ $admins }}</h5>
+                                <div class="text font-20">الصلاحيات</div>
+                                <h5 class="number">{{ $roles }}</h5>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6">
+                <div class="col-lg-3 col-md-6">
                     <div class="card top_counter">
                         <div class="body">
-                            <div class="icon xl-slategray"><i class="zmdi zmdi-city-alt"></i> </div>
+                            <div class="icon xl-slategray"><i class="zmdi zmdi-collection-text"></i> </div>
                             <div class="content">
-                                <div class="text font-20">أصحاب العمل</div>
-                                <h5 class="number">{{ $admins }}</h5>
+                                <div class="text font-20">المجالات</div>
+                                <h5 class="number">{{ $categories }}</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="card top_counter">
+                        <div class="body">
+                            <div class="icon xl-slategray"><i class="zmdi zmdi-pin"></i> </div>
+                            <div class="content">
+                                <div class="text font-20">المحافظات</div>
+                                <h5 class="number">{{ $regions }}</h5>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="row clearfix">
-                <div class="col-lg-4 col-md-6">
+                <div class="col-lg-3 col-md-6">
+                    <div class="card top_counter">
+                        <div class="body">
+                            <div class="icon xl-slategray"><i class="zmdi zmdi-city-alt"></i> </div>
+                            <div class="content">
+                                <div class="text font-20">أصحاب العمل</div>
+                                <h5 class="number">{{ $employers }}</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="card top_counter">
+                        <div class="body">
+                            <div class="icon xl-slategray"><i class="zmdi zmdi-account-box"></i> </div>
+                            <div class="content">
+                                <div class="text font-20">الباحثين عن عمل</div>
+                                <h5 class="number">{{ $jobSeekers }}</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
                     <div class="card top_counter">
                         <div class="body">
                             <div class="icon xl-slategray"><i class="zmdi zmdi-case"></i> </div>
                             <div class="content">
                                 <div class="text font-20">الوظائف</div>
-                                <h5 class="number">{{ $admins }}</h5>
+                                <h5 class="number">{{ $jobs }}</h5>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6">
+                <div class="col-lg-3 col-md-6">
                     <div class="card top_counter">
                         <div class="body">
-                            <div class="icon xl-slategray"><i class="zmdi zmdi-developer-board"></i> </div>
+                            <div class="icon xl-slategray"><i class="zmdi zmdi-chart-donut"></i> </div>
                             <div class="content">
-                                <div class="text font-20">التدريب</div>
-                                <h5 class="number">19</h5>
+                                <div class="text font-20">التدريبات</div>
+                                <h5 class="number">{{ $trainings }}</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row clearfix">
+                <div class="col-lg-3 col-md-6">
+                    <div class="card top_counter">
+                        <div class="body">
+                            <div class="icon xl-slategray"><i class="zmdi zmdi-accounts"></i> </div>
+                            <div class="content">
+                                <div class="text font-20">الفرق</div>
+                                <h5 class="number">{{ $teams }}</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="card top_counter">
+                        <div class="body">
+                            <div class="icon xl-slategray"><i class="zmdi zmdi-collection-plus"></i> </div>
+                            <div class="content">
+                                <div class="text font-20">طلبات التقديم</div>
+                                <h5 class="number">{{ $applications }}</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="card top_counter">
+                        <div class="body">
+                            <div class="icon xl-slategray"><i class="zmdi zmdi-comment-text"></i> </div>
+                            <div class="content">
+                                <div class="text font-20">الاستفسارات</div>
+                                <h5 class="number">{{ $inquires }}</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="card top_counter">
+                        <div class="body">
+                            <div class="icon xl-slategray"><i class="zmdi zmdi-email"></i> </div>
+                            <div class="content">
+                                <div class="text font-20">الرسائل</div>
+                                <h5 class="number">{{ $messages }}</h5>
                             </div>
                         </div>
                     </div>
