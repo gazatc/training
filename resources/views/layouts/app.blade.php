16e44b58955faed02819a457487beb8d7eb03d37
Abdullah Shublaq <abd.shublaq@gmail.com>
2021-06-15 12:51:55 +0300

    fix front routes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
     <title>وظائف غزة</title>
+    <link rel="icon" href="{{asset('suitcase.svg')}}" type="image/x-icon"> <!-- Favicon-->
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
